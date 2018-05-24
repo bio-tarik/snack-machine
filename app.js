9b6930d0ccffb942f5f4f0b763706016c89c3971
bio-tarik <bio.tarik@gmail.com>
2018-05-24 04:27:24 -0300

    Removing views from project

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const exphbs = require('express-handlebars');
 const path = require('path');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
@@ -15,20 +14,13 @@ client.on('connect', () => {
 const port = 3000;
 const app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
 app.get('/', (req, res, next) => {
-    res.render('search');
+    res.json({ "message": "Snack machine is alive!" });
 });
-
-app.post('', (req, res, next) => {
-    let id = req.body.id;
-})
 
 app.listen(port, () => {
     console.log('Server start on port ' + port);
